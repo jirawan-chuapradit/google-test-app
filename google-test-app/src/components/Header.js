b11f6b88bef2d4784467a44315194c8c572f764e
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2022-03-12 18:08:49 +0700

    add header

---
+++ google-test-app/src/components/Header.js
@@ -0,0 +1,24 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div class="bar">
+        <a href="" className="item2">
+          Gmail
+        </a>
+        <a href="" className="item2">
+          ค้นรูป
+        </a>
+      </div>
+
+      <div class="bar">
+        <a href="" className="item2">
+          แอป google
+        </a>
+        <img src="https://pbs.twimg.com/profile_images/805775577420468224/ix8EMEKv_400x400.jpg" alt="บัญชี google"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
