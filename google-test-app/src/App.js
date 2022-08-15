2a9914d421d3e962d5d4646ad17e576acf6c7d8b
jirawan.c <jirawan-chuapradit@users.noreply.github.com>
2022-08-15 16:13:27 +0700

    add navbar

--- google-test-app/src/App.js
+++ google-test-app/src/App.js
@@ -1,11 +1,12 @@
 import Footer from "./components/Footer";
 import "./App.css";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <Footer />
     </div>
   );
---
+++ google-test-app/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import "./Navbar.css";
+import Profile from "../images/profile.jpg"
+const Navbar = () => {
+  return (
+    <div className="navbar">
+    
+      <div class="rightLinks">
+        <a href="/gmail" className="right-item">
+          Gmail
+        </a>
+        <a href="/images" className="right-item">
+          ค้นรูป
+        </a>
+        <svg focusable='false' viewBox='0 0 24 24' className="right-item">
+            <path d='M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z' />
+        </svg>
+        <img src={Profile} alt="บัญชี google" className="right-item"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
