ece7500128af80e0f3f2fe4c83606d919e836efd
jirawan.c <jirawan-chuapradit@users.noreply.github.com>
2022-09-08 10:54:07 +0700

    log search

--- google-test-app/src/components/Home.js
+++ google-test-app/src/components/Home.js
@@ -3,10 +3,13 @@ import "./Home.css"
 import googleLogo from '../images/logo_google.png'
 
 function Home() {
+    const search = (event)=>{
+        console.log(event.target.value)
+    }
     return (
         <div className="container">
             <img src={googleLogo} alt="Google" className="logo"></img>
-            <input className="search" id="input" type="search" autocomplete="off" spellcheck="false"  placeholder="Search Google or type a URL"></input>
+            <input className="search" id="input" type="search"  placeholder="Search Google or type a URL" onChange={search}></input>
         </div>
        
     )
